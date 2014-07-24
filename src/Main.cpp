e0bb3d2a8dfbb19d1f457bbc39131a1597ea1777
Michal Zajacik <michal.zajacik@agents.fel.cvut.cz>
2014-07-24 17:48:18 +0200

    first commit

---
+++ src/Main.cpp
@@ -0,0 +1,18 @@
+//============================================================================
+// Name        : PlayFiled.cpp
+// Author      : Michal Zajacik
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include "Main.hpp"
+
+int main() {
+
+	int result = day9();
+
+	return result;
+}
+
---
+++ src/Main.hpp
@@ -0,0 +1,13 @@
+/*
+ * Main.hpp
+ *
+ *  Created on: Jul 24, 2014
+ *      Author: michal
+ */
+
+#ifndef MAIN_HPP_
+#define MAIN_HPP_
+
+#include "day9.hpp"
+
+#endif /* MAIN_HPP_ */
